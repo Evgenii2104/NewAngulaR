812cdb84b557c3300928fd525fa8e29e83f862a9
evgenii2104 <evgenii2104@rambler.ru>
2023-06-16 00:44:59 +0300

    service

--- ng-course/src/app/app.component.ts
+++ ng-course/src/app/app.component.ts
@@ -1,42 +1,21 @@
 import { Component } from '@angular/core';
-import {delay, Observable, of} from "rxjs";
-
 
 @Component({
   selector: 'app-root',
-  template: `<div class="col-xs-8 col-xs-offset-2">
-    <h1>{{ asyncTitle | async }}</h1>
-    <input type="text" class="form-control" [(ngModel)]="searchCar">
-    <button class="btn btn-primary" (click)="addCar()">Добавить</button>
-    <hr>
-    <ul class="list-group">
-      <li class="list-group-item" *ngFor="let car of cars | carFilter: searchCar:'name'; let i = index">
-        <b>{{ i + 1 }}</b> {{ car.name}}
-        <i>{{ car.descr}}</i>
-      </li>
-    </ul>
-  </div> `
-  //styleUrls: ['./app.component.css']
+  templateUrl: `app.component.html`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  searchCar = ''
   cars = [
-    {name: 'Ford', descr: 'wf 1'},
-    {name: 'Mazda', descr: 'wf 2'},
-    {name: 'Bentley', descr: 'wf 3'},
-    {name: 'Audi', descr: 'wf 4'},
-    {name: 'Mercedes', descr: 'wf 5'},
-    {name: 'BMW', descr: 'wf 6'}
+    {name: 'Ford', isSold: false},
+    {name: 'Mazda', isSold: true},
+    {name: 'Mercedes', isSold: false}
   ]
 
-  asyncTitle = of('Async title 3 seconds')
-    .pipe(delay(3000));
-
-
-  addCar() {
+  addCarToList(carName: any) {
     this.cars.push({
-      name: 'New Car',
-      descr: 'wf'
+      name: carName,
+      isSold: false
     })
   }
 }
--- ng-course/src/app/app.module.ts
+++ ng-course/src/app/app.module.ts
@@ -3,17 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import {PowPipe} from "./pow.pipe";
-import {CarFilterPipe} from "./car-filter.pipr";
-
-
+import {CarComponent} from "./car/car.component";
+import {CarAddComponent} from "./car-add/car-add.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PowPipe,
-    CarFilterPipe
+    CarComponent,
+    CarAddComponent
   ],
     imports: [
         BrowserModule,
