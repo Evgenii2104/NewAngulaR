00e213c5b13b9e5be089d49bbaf21f1c0956f9c8
evgenii2104 <evgenii2104@rambler.ru>
2023-06-15 23:27:40 +0300

    pipe1

--- ng-course/src/app/app.component.ts
+++ ng-course/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {delay, Observable, of} from "rxjs";
+
 
 @Component({
   selector: 'app-root',
   template: `<div class="col-xs-8 col-xs-offset-2">
+    <h1>{{ asyncTitle | async }}</h1>
     <input type="text" class="form-control" [(ngModel)]="searchCar">
+    <button class="btn btn-primary" (click)="addCar()">Добавить</button>
     <hr>
     <ul class="list-group">
       <li class="list-group-item" *ngFor="let car of cars | carFilter: searchCar:'name'; let i = index">
@@ -24,4 +28,15 @@ export class AppComponent {
     {name: 'Mercedes', descr: 'wf 5'},
     {name: 'BMW', descr: 'wf 6'}
   ]
+
+  asyncTitle = of('Async title 3 seconds')
+    .pipe(delay(3000));
+
+
+  addCar() {
+    this.cars.push({
+      name: 'New Car',
+      descr: 'wf'
+    })
+  }
 }
--- ng-course/src/app/car-filter.pipr.ts
+++ ng-course/src/app/car-filter.pipr.ts
@@ -1,10 +1,12 @@
 import {Pipe, PipeTransform} from "@angular/core";
 
 @Pipe({
-  name: 'carFilter'
+  name: 'carFilter',
+  pure: false
 })
 export class CarFilterPipe implements PipeTransform {
   transform(carList: any, searchStr: string, fieldName: string) {
+    console.log('Filter pipe started')
     if (carList.length === 0 || searchStr === '') {
       return carList
     }
