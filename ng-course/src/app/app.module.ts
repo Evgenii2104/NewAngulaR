d45fb7924e3e5d4601de8b541976382d2105ac6b
evgenii2104 <evgenii2104@rambler.ru>
2023-07-04 23:34:03 +0300

    animation

--- ng-course/src/app/app.module.ts
+++ ng-course/src/app/app.module.ts
@@ -4,24 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {HomePageComponent} from "./home-page/home-page.component";
-import {AppRoutingModule} from "./app-routing.module";
-import {HeaderComponent} from "./header/header.component";
-import {SharedModule} from "./shared/shared.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    HeaderComponent,
   ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        SharedModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
